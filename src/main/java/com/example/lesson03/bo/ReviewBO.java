3216c4e0825ac315384ce5dc08778c28600e2f2f
muriol <test@test.com>
2023-12-19 18:43:09 +0900

    12-19 update 예제 + insert 예제

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -10,8 +10,25 @@ import com.example.lesson03.mappers.ReviewMapper;
 public class ReviewBO {
 	@Autowired
 	private ReviewMapper reviewMapper;
+	
 	//input : x      //output : Review 한개의 데이터만 컨트롤러에게 준다.
 	public Review getReview (int id) {
 		return reviewMapper.selectReview(id);
+	}
+	
+	//in : review    out: 성공한 행의 개수(int)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	// addReviewAsField
+	//(4, "콤비네이션R", "바다", 5.0, "역시 맛있다!");
+	public int addReviewAsField(int storeId, String menu, 
+			String userName, Double point, String review) {
+		return reviewMapper.insertReviewAsField(storeId, menu, userName, point, review);
+	}
+	// input: id, review  output: int 성공한 행 개수
+	public int updateReviewById(int id, String review) {
+		return reviewMapper.updateReviewById(id, review);
 	}
 }
--- src/main/java/com/example/lesson03/mappers/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mappers/ReviewMapper.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.mappers;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.domain.Review;
@@ -7,4 +8,20 @@ import com.example.lesson03.domain.Review;
 @Repository
 public interface ReviewMapper {
 	public Review selectReview(int id);
+	
+	//input : review    output: 성공한 행의 개수(int)
+	public int insertReview(Review review);
+	
+	public int insertReviewAsField(
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu, 
+			@Param("userName") String userName, 
+			@Param("point") Double point, 
+			@Param("review") String review);
+	
+	// input: id, review  output: int 성공한 행 개수
+	public int updateReviewById(
+			@Param("id") int id, 
+			@Param("review") String review);
 }
+	
